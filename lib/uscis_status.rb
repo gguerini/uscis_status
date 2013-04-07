b34d03585ee0ca425ba47189808e58197c29394f
Guillermo Guerini <guillermo@gguerini.com>
2013-04-07 16:36:27 -0700

    Move the logic to a different class to improve abstraction.

--- lib/uscis_status.rb
+++ lib/uscis_status.rb
@@ -3,55 +3,64 @@ require 'mechanize'
 require 'nokogiri'
 
 module USCISStatus
-  CURRENT_CASE = "Your Current Case Status for"
 
-  def self.check(application_numbers)
+  class USCISWebScraping
+    CURRENT_CASE = "Your Current Case Status for"
 
-    # Check if the parameter is an Array, otherwise create one
-    applications = application_numbers.kind_of?(Array) ? application_numbers : application_numbers.split
+    def self.check(application_numbers)
+      # Check if the parameter is an Array, otherwise create one
+      applications = application_numbers.kind_of?(Array) ? application_numbers : application_numbers.split
 
-    a = Mechanize.new
-    statuses = []
+      a = Mechanize.new
+      statuses = []
 
-    applications.each do |number|
-      next if number.nil? or number.empty?
+      applications.each do |number|
+        next if number.nil? or number.empty?
 
-      page = a.post("https://egov.uscis.gov/cris/Dashboard/CaseStatus.do", { "appReceiptNum" => number })
+        page = a.post("https://egov.uscis.gov/cris/Dashboard/CaseStatus.do", { "appReceiptNum" => number })
 
-      # Look for possible errors with this application number
-      error = page.search('div.errorContainer > ul')
-      if !error.empty?
-        statuses << {number: number, type: '', status: error.text.strip, description: '', general_description: ''}
-        next
+        # Look for possible errors with this application number
+        error = page.search('div.errorContainer > ul')
+        if !error.empty?
+          statuses << {number: number, type: '', status: error.text.strip, description: '', general_description: ''}
+          next
+        end
+
+        # Get the application type and description (eg. Form I130...)
+        application_type = capitalize_words(page.search('div#caseStatus>h3').text.gsub(CURRENT_CASE, ""))
+
+        # Get current application block
+        current_application = page.search('div.caseStatusInfo')
+
+        # Get the Status
+        status = current_application.search('h4').text.strip
+
+        # Get the Description
+        description = current_application.search('.caseStatus').text.strip
+
+        # Get the General Description for the Application
+        general_description = current_application.search('#bucketDesc').text.strip
+
+        statuses << {number: number, type: application_type, status: status, description: description, general_description: general_description}
+
       end
 
-      # Get the application type and description (eg. Form I130...)
-      application_type = capitalize_words(page.search('div#caseStatus>h3').text.gsub(CURRENT_CASE, ""))
-
-      # Get current application block
-      current_application = page.search('div.caseStatusInfo')
-
-      # Get the Status
-      status = current_application.search('h4').text.strip
-
-      # Get the Description
-      description = current_application.search('.caseStatus').text.strip
-
-      # Get the General Description for the Application
-      general_description = current_application.search('#bucketDesc').text.strip
-
-      statuses << {number: number, type: application_type, status: status, description: description, general_description: general_description}
+      return statuses
 
     end
 
-    return statuses
+    private
+
+    def self.capitalize_words(sentence)
+      sentence.strip.split(' ').map{ |word| word.capitalize }.join(' ')
+    end
 
   end
 
-  private
+  def self.check(application_numbers)
 
-  def self.capitalize_words(sentence)
-    sentence.strip.split(' ').map{ |word| word.capitalize }.join(' ')
+    return USCISWebScraping.check(application_numbers)
+
   end
 
 end
